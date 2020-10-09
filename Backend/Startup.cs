670911f057fc55d93643a43a6c27db7c0b73057e
Carlos <skegtyko@gmail.com>
2020-10-09 11:47:56 -0300

    Casca

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -36,6 +36,11 @@ namespace Backend
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(
+                x => x.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod());
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
