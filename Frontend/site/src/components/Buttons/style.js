5ac5fa8b696bd5adc22ef7cc69baceef736307c1
BrenninBR <brennin.chave@gmail.com>
2020-10-15 13:38:42 -0300

    Atualizado

---
+++ Frontend/site/src/components/Buttons/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ButtonStyled = styled.button`
+  color: white;
+  font-size: 1em;
+  min-width: 150px;
+  background-color: green;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid green;
+  border-radius: 3px;
+  cursor: pointer;
+  opacity: 0.6;
+  transition: 0.3s;
+`;
+
+
+    
--- Frontend/site/src/index.js
+++ Frontend/site/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Button from './components/Buttons/index';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Button />
   </React.StrictMode>,
   document.getElementById('root')
 );
