5c56fde2c70080438cfb2693a7c6edad340ba667
BrenninBR <brennin.chave@gmail.com>
2020-10-22 19:33:43 -0300

    asdads

---
+++ Frontend/site/src/pages/Lanche/Pipoca/style.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+`;
+
+export const WrapperSecondary = styled.div`
+
+    display: inline-flexbox;
+    flex-direction: column;
+    
+    justify-content: space-between;
+    align-items: center;
+
+    margin-top: 10px;
+    width: 500px;
+`;
+
+export const SelectWrapper = styled.div`
+
+    display: flex;
+    flex-direction: column;
+`;
+
+export const LancheWrapper = styled.div`
+
+    display: flex;
+    flex-direction: column;
+
+    float: left;
+
+    width: 650px;
+`;
+
+export const ButtonWrapper = styled.div`
+
+    display: inline-flexbox;
+    flex-direction: column;
+`;
+
