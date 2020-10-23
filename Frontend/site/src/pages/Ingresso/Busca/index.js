e22508e3954bffefbbf3f8e06ada8aca32ae5c87
weverton-marcos-svg <wevertonmarcos.842@gmail.com>
2020-10-23 03:11:39 -0300

    Restauração Front --Done

--- Frontend/site/src/pages/Ingresso/Busca/index.js
+++ Frontend/site/src/pages/Ingresso/Busca/index.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { Wrapper, SearchWrapper, Input, SelectionWrapper } from './style';
+import Relogio from '../../../components/Relogio/';
+import SelectionFilme from '../../../components/SelectionFilme/';
+import SelectFilme from '../../../components/SelectionFilme/';
 
 export default function Busca (){
     return (
-        <div></div>
+        <Wrapper>
+            <h1>Filmes em Cartaz</h1>  
+
+            <SearchWrapper>
+                    <Input type="text" size="45" placeholder="Pesquisar filme" />
+                    <Relogio />   
+            </SearchWrapper>
+
+            <SelectionWrapper>
+                <div>
+                    <select>
+                        <option>Livre</option>
+                        <option>12</option>
+                        <option>14</option>
+                        <option>16</option>
+                        <option>18</option>
+                    </select>
+                </div>
+
+                <div>
+                    <select>
+                        <option>Comédia</option>
+                        <option>Romance</option>
+                        <option>Ação</option>
+                        <option>Suspense</option>
+                        <option>Terror</option>
+                    </select>
+                </div>
+
+                <div>
+                    <select>
+                        <option>3D</option>
+                    </select>
+                </div>
+            </SelectionWrapper>
+
+            <SelectFilme />
+            <SelectFilme />
+            <SelectFilme />
+            <SelectFilme />
+        </Wrapper>
     );
 }
--- Frontend/site/src/pages/Pagamento/Dinheiro/index.js
+++ Frontend/site/src/pages/Pagamento/Dinheiro/index.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Button from '../../../components/Buttons/';
+import Relogio from '../../../components/Relogio/';
+import { TextWrapper, Wrapper } from './style';
 
 export default function Dinheiro (){
     return (
-        <div></div>
+        <Wrapper>
+            <h1>Pagamento com Dinheiro</h1>
+            <Relogio />
+            <TextWrapper>
+                <p>
+                    Não deseja pagar no cartão ou por qrcode? Tudo bem, pague com dinheiro no momento que for retirar seu pedido,
+                    no momento da retirar seu lanche pague tudo de uma so vez.
+                </p>  
+
+                <p> 
+                    Lembre-se que ao comprar o ingresso de meia-entrada deverá mostrar um documento
+                    de que você tem direito a esse ingresso caso contrario deverá pagar o restante no caixa.
+                </p>
+            </TextWrapper>
+            <div>
+                <h2>N° Pedido:</h2>
+            </div>
+
+            <div>
+                <Button to="" onClick="" children="Cancelar Pedido"/>
+                <Button to="" onClick="" children="Finalizar"/>
+            </div>
+        </Wrapper>
+
     );
 }
