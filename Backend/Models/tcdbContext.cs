47a16d9dd1b2eed42ea9c8225e309b334279c77d
infoc20 <infoc20@edu.frei>
2020-10-26 15:59:12 -0300

    Subindo arquivos do Services

--- Backend/Models/tcdbContext.cs
+++ Backend/Models/tcdbContext.cs
@@ -36,8 +36,7 @@ namespace Backend.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySql("server=localhost;userid=root;password=1234;database=tcdb", x => x.ServerVersion("8.0.21-mysql"));
+                optionsBuilder.UseMySql("server=localhost;userid=adm;password=12345678;database=tcdb", x => x.ServerVersion("8.0.21-mysql"));
             }
         }
 
---
+++ Frontend/site/src/services/CartaoApi.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL:'http://54.174.164.124:5000'
+});
+
+export default class Cartao {
+    
+}
