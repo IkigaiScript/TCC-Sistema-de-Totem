b5150355be6e181bb7ae7d448f8abc4e89b2e5d8
Carlos <skegtyko@gmail.com>
2020-11-13 08:16:12 -0300

    Services do gerente

---
+++ Frontend/site/src/services/GerenteApi.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL:'http://54.89.253.106:5000/'
+})
+
+export class Gerente {
+    async TopFilmes(){
+        const response = await api.get(`Gerente/Top/Filmes`);
+        return response.data
+    }
+
+    async VendasDoDia(dia){
+        const response = await api.get(`Gerente/Vendas/Dia/${dia}`);
+        return response.data;
+    }
+
+    async VendasdoMes(inicio,final){
+        const response = await api.get(`Gerente/Venda?inicio=${inicio}&final=${final}`);
+        return response.data;
+    }
+
+    async TopProdutos(){
+        const response = await api.get(`Gerente/Top/Produtos`);
+        return response.data;
+    }
+}
